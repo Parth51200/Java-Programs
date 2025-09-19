b0687f7119e8494614e387b99f09a0329ae4a8f8
Parth51200 <parthmohite1511@gmail.com>
2025-09-19 11:59:58 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile{
+    public static void main(String[] args) {
+        int i = 1; // initialization
+
+        do {
+            System.out.println(i);
+            i++; // increment
+        } while (i <= 5); // condition checked after loop body
+    }
+}
+
+    
+
