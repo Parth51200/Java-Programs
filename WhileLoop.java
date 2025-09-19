b163ea612ce6240b663ac338904b5ec6b7777f16
Parth51200 <parthmohite1511@gmail.com>
2025-09-19 11:39:04 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1; // initialization
+
+        while (i <= 5) { // condition
+            System.out.println(i);
+            i++; // increment to avoid infinite loop
+        }
+    }
+}
